99782b21260e26f99bc52d28995d146f6cd8c320
swadesh Ranjan Swain <swadeshswain@gmail.com>
2018-06-08 10:50:36 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<style>
+body {
+    margin: 0;
+}
+</style>
+	<meta charset="UTF-8"/>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Vrushali&#039;s Slimming Center &#8211; The Healthy Way Is The Right Way</title>
+</head>
+<body>
+<iframe src="http://google.co.in/" style="border: 0; width: 100%; height: 100%">Your browser doesn't support iFrames.</iframe>
+</body>
+</html>
+
